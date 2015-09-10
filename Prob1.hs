28bdb68b4da1e66a43e5554cecd15cccf8da50a7
momo2ro <momo2ro.const@gmail.com>
2015-09-10 12:17:03 +0900

    命名力

--- Prob1.hs
+++ Prob1.hs
@@ -1,5 +1,7 @@
-isMultiple xs = if null xs
-                    then 0
-                    else if mod (head xs) 3 == 0 || mod (head xs) 5 == 0
-                        then (head xs) + (isMultiple (tail xs))
-                        else isMultiple (tail xs)
+isMultiple xs =
+        let n = head xs
+        in if null xs
+           then 0
+            else if mod n 3 == 0 || mod n 5 == 0
+                then n + (isMultiple (tail xs))
+                else isMultiple (tail xs)
