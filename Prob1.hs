4fa7f754d8c59f747ffe475080875f848528e81a
momo2ro <momo2ro.const@gmail.com>
2015-09-10 12:21:06 +0900

    modified:   Prob1.hs

--- Prob1.hs
+++ Prob1.hs
@@ -1,7 +1,7 @@
-isMultiple xs =
+sumMultiple xs =
         let n = head xs
         in if null xs
            then 0
-            else if mod n 3 == 0 || mod n 5 == 0
-                then n + (isMultiple (tail xs))
-                else isMultiple (tail xs)
+           else if mod n 3 == 0 || mod n 5 == 0
+                then n + (sumMultiple (tail xs))
+                else sumMultiple (tail xs)
